cc97b95a7f4191a1fb13a65014df8b5a173fcf04
Saba mohebpour <sabamm@Sabas-MacBook-Air.local>
2014-08-04 11:56:18 -0700

    added a test class

---
+++ ccTest.h
@@ -0,0 +1,13 @@
+//
+//  ccTest.h
+//  Git Test
+//
+//  Created by Saba mohebpour on 2014-08-04.
+//  Copyright (c) 2014 ___saba Company___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ccTest : UITableViewController
+
+@end
